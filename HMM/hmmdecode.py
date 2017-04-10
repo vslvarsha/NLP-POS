7340007807aa3bfd78d347d124b75cbd97635901
vslvarsha <varsha@guest-wireless-upc-1604-10-120-066-124.usc.edu>
2017-04-10 13:50:10 -0700

    Data pre-processing and POS tagging

---
+++ HMM/hmmdecode.py
@@ -0,0 +1,178 @@
+import sys
+import time
+import math
+import ast
+import operator
+
+def POStagging(data,t,e,count):
+    fout = open('hmmoutput.txt', 'w+')
+    for line in data:
+        state = 0
+        value = 0
+        previous = 'q0'
+        back_pointer = {}
+        state_prob = {}
+        words = line.split()
+        for word in words:
+            if word not in e:
+                for tag in count:
+                    maxvalue = -9999
+                    if previous != 'q0':
+                        if previous not in e:
+                            previous_state_list = count.keys()
+                        else:
+                            previous_state_list = e[previous]
+                        for p in previous_state_list:
+                            if t[p][tag] != 0:
+                                value = state_prob[state-1][p] + math.log(t[p][tag], 10)
+                            if maxvalue < value:
+                                maxvalue = value
+                                if state not in state_prob:
+                                    state_prob[state] = {}
+                                    state_prob[state][tag] = value
+                                else:
+                                    state_prob[state][tag] = value
+
+                                if state not in back_pointer:
+                                    back_pointer[state] = {}
+                                    back_pointer[state][tag] = p
+                                else:
+                                    back_pointer[state][tag] = p
+                    else:
+                        if t['q0'][tag] != 0:
+                            value = math.log(t['q0'][tag],10)
+                        if state not in state_prob:
+                            state_prob[state] = {}
+                            state_prob[state][tag] = value
+                        else:
+                            state_prob[state][tag] = value
+
+                        if state not in back_pointer:
+                            back_pointer[state] = {}
+                            back_pointer[state][tag] = 'q0'
+                        else:
+                            back_pointer[state][tag] = 'q0'
+
+            else:
+                for tag in e[word]:
+                    maxvalue = -9999
+                    if previous != 'q0':
+                        if previous not in e:
+                            previous_state_list = count.keys()
+                        else:
+                            previous_state_list = e[previous]
+                        for p in previous_state_list:
+                            if t[p][tag] !=0 and e[word][tag]!= 0:
+                                value = state_prob[state - 1][p] + math.log(e[word][tag], 10) + math.log(t[p][tag], 10)
+                            if maxvalue < value:
+                                maxvalue = value
+                                if state not in state_prob:
+                                    state_prob[state] = {}
+                                    state_prob[state][tag] = value
+                                else:
+                                    state_prob[state][tag] = value
+
+                                if state not in back_pointer:
+                                    back_pointer[state] = {}
+                                    back_pointer[state][tag] = p
+                                else:
+                                    back_pointer[state][tag] = p
+                    else:
+                        if t['q0'][tag] != 0 and e[word][tag] != 0:
+                            value = math.log(e[word][tag], 10) + math.log(t['q0'][tag], 10)
+                        if state not in state_prob:
+                            state_prob[state] = {}
+                            state_prob[state][tag] = value
+                        else:
+                            state_prob[state][tag] = value
+
+                        if state not in back_pointer:
+                            back_pointer[state] = {}
+                            back_pointer[state][tag] = 'q0'
+                        else:
+                            back_pointer[state][tag] = 'q0'
+            state = state +1
+            previous = word
+            #print back_pointer
+            #print state_prob[state-1]
+
+        tagged_data = ''
+        temp = max(state_prob[state-1].iteritems(), key=operator.itemgetter(1))[0]
+        tagged_data = words[state-1]+'/'+temp
+        for i in range(len(words)-2, -1, -1):
+            temp = back_pointer[i+1][temp]
+            tagged_data += ' '+ words[i] + '/' + temp
+        tagged_data = ' '.join(reversed(tagged_data.split()))
+        #print tagged_data
+        fout.write(tagged_data)
+        fout.write('\n')
+    fout.close()
+
+
+def F1_score(tags,predicted):
+    divide = len(tags)
+    tags = set(tags)
+    predicted = set(predicted)
+
+    accuracy = len(tags.intersection(predicted))/float(divide)
+
+    return (1-accuracy)
+
+def main():
+    start = time.clock()
+    '''if len(sys.argv) != 2:
+        print "Missing the location of input data"
+        sys.exit(1)
+    else:
+        fin = open(sys.argv[1], 'r')'''
+
+    #reading untagged data
+    fin = open('kannada_corpus_dev_raw.txt','r')
+    #fin = open('catalan_corpus_dev_raw.txt', 'r')
+    untagged_data = fin.readlines()
+    fin.close()
+
+    #reading model parameters
+    fmodel = open('hmmmodel.txt','r')
+    lines = fmodel.readlines()
+    tag_count = ast.literal_eval(lines[0])
+    emission_probability = ast.literal_eval(lines[1])
+    transition_probability = ast.literal_eval(lines[2])
+    fmodel.close()
+
+    #POS tagging using Viterbi algorithm and writing to file
+    POStagging(untagged_data, transition_probability, emission_probability, tag_count)
+
+
+    tag = []
+    predicted = []
+    fout  = open('junk.txt','w')
+    f1 = open('kannada_corpus_dev_tagged.txt', 'r')
+    dev_tagged = f1.readlines()
+    for line in dev_tagged:
+        words = line.split()
+        for word in words:
+            tag.append(word[word.rfind('/')+1:])
+    f2 = open('hmmoutput.txt', 'r')
+    train_tagged = f2.readlines()
+    for line in train_tagged:
+        words = line.split()
+        for word in words:
+            predicted.append(word[word.rfind('/') + 1:])
+    fout.write(str(tag))
+    fout.write(str(predicted))
+    print "Accuracy"
+    print F1_score(tag, predicted)
+
+
+    fout.close()
+    f1.close()
+    f2.close()
+
+
+    print "Time taken"
+    print time.clock()-start
+
+if __name__ == '__main__':
+    main()
+
---
+++ HMM/hmmlearn.py
@@ -0,0 +1,127 @@
+import sys
+import time
+
+def getTaggedData(fin):
+    tagged_data = []
+    lines = fin.readlines()
+    for line in lines:
+        words = line.split()
+        for word in words:
+            tagged_data.append(word)
+    return tagged_data
+
+
+def getTagCount(tagged_data):
+    tag_count = {}
+    for word in tagged_data:
+        curent_word = word[:word.rfind('/')]
+        tag = word[word.rfind('/')+1:]
+        if tag not in tag_count:
+            tag_count[tag] = 1
+        else:
+            tag_count[tag] += 1
+    return tag_count
+
+def getEmissionProbability(tagged_data, tag_count):
+    wordplustag = {}
+    for word in tagged_data:
+        current_word = word[:word.rfind('/')]
+        tag = word[word.rfind('/') + 1:]
+
+        if current_word not in wordplustag:
+            wordplustag[current_word]={}
+            wordplustag[current_word][tag] = 1
+        else:
+            if tag not in wordplustag[current_word]:
+                wordplustag[current_word][tag]=1
+            else:
+                wordplustag[current_word][tag]+=1
+
+    for word in wordplustag:
+        for t in wordplustag[word]:
+            wordplustag[word][t] = (wordplustag[word][t] * 1.0) / tag_count[t]
+    return wordplustag
+
+
+def getTransitionProbability(raw_data, tag_count):
+    transition = {}
+    for line in raw_data:
+        previous = ''
+        words = line.split()
+        for word in words:
+            current_word = word[:word.rfind('/')]
+            tag = word[word.rfind('/') + 1:]
+
+            if previous == '':
+                current = 'q0'
+            else:
+                current = previous
+
+            if current not in transition:
+                transition[current] = {}
+                transition[current][tag] = 1
+            else:
+                if tag not in transition[current]:
+                    transition[current][tag] = 1
+                else:
+                    transition[current][tag] += 1
+            previous = tag
+
+    #print transition
+    for tag in transition:
+        flag = 0
+        total = 0
+        for t in tag_count:
+            if t not in transition[tag]:
+                transition[tag][t] = 0
+                flag = 1
+        if flag == 1:
+            for t in tag_count:
+                transition[tag][t] += 1
+        total = sum(transition[tag].values())
+
+        for current in transition[tag]:
+            transition[tag][current] = (transition[tag][current] * 1.0) / (total)
+
+    return transition
+
+def main():
+    start = time.clock()
+    '''if len(sys.argv) != 2:
+        print "Missing the location of input data"
+        sys.exit(1)
+    else:
+        f1 = sys.argv[1]
+        fin = open(f1, 'r')'''
+
+    #fin = open('catalan_corpus_train_tagged.txt','r')
+    fin = open('kannada_coprus_train_tagged.txt','r')
+    #fin = open('kannada_coprus_train_tagged.txt','r')
+
+    tagged_data = getTaggedData(fin)
+    #print tagged_data
+    tag_count = getTagCount(tagged_data)
+    print tag_count
+    print len(tag_count)
+
+    fin.seek(0)
+    raw_data = fin.readlines()
+    transition_probability = getTransitionProbability(raw_data, tag_count)
+    print transition_probability
+    emission_probability = getEmissionProbability(tagged_data, tag_count)
+    print emission_probability
+
+    fout = open('hmmmodel.txt','w')
+    fout.write(str(tag_count))
+    fout.write('\n')
+    fout.write(str(emission_probability))
+    fout.write('\n')
+    fout.write(str(transition_probability))
+    fout.close()
+
+    fin.close()
+    print "Time taken"
+    print (time.clock()-start)
+
+if __name__ == '__main__':
+    main()
